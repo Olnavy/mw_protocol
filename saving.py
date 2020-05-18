b1b0449f584dc601c0b68ef44219c237103b7767
Yvan Romé <eeymr@leeds.ac.uk>
2020-05-18 18:59:02 +0100

    Factorisation of saving. To be continued.

--- saving.py
+++ saving.py
@@ -5,225 +5,41 @@ import numpy.ma as ma
 import os
 import datetime
 
-
 # --------------------------------- #
 # ---------- MAIN METHOD ---------- #
 # --------------------------------- #
 
-def saving(discharge_mw, ds_lsm, lsm_name, mode):
+output_folder = "/nfs/annie/eeymr/work/outputs/proj_glac_mw"
+
+
+def saving(discharge_mw, ds_lsm, lsm_name, mode, start_year=-26, end_year=0, step=100, mode_smooth="diff"):
+    
     print("__ Saving algorithm")
     
     lsm, longitude, latitude = ds_lsm.lsm.values, ds_lsm.longitude.values, ds_lsm.latitude.values
     
     if mode == "routed":
-        routed_to_netcdf(discharge_mw, lsm, longitude, latitude, lsm_name)
+        folder_path, file_path, title = routed_to_netcdf(start_year, end_year, step, mode_smooth, lsm_name)
     elif mode == "spreaded":
-        spreaded_to_netcdf(discharge_mw, lsm, longitude, latitude, lsm_name)
+        folder_path, file_path, title = spreaded_to_netcdf(start_year, end_year, step, mode_smooth, lsm_name)
     elif mode == "corrected":
-        corrected_to_netcdf(discharge_mw, lsm, longitude, latitude, lsm_name)
+        folder_path, file_path, title = corrected_to_netcdf(start_year, end_year, step, mode_smooth, lsm_name)
     else:
         print("The mode wasn't recognized.")
-
-
-def fixing(ds_ref, mode_fixing, mode_smooth, start, end, lsm_name, corrected=False):
-    print("__ Fixing algorithm")
-    
-    ref_mw, longitude, latitude, t_ref = \
-        ds_ref.discharge.values, ds_ref.longitude.values, ds_ref.latitude.values, ds_ref.t.values
-    
-    if mode_fixing == "time":
-        processed_mw, processed_time = process_time(ref_mw, start, end, t_ref)
-        processed_to_netcdf(processed_mw, processed_time, longitude, latitude, mode_smooth, start, end, lsm_name,
-                            corrected)
-    else:
-        print("The mode wasn't recognized.")
-
-
-# ------------------------------------ #
-# ---------- SAVING METHODS ---------- #
-# ------------------------------------ #
-
-def routed_to_netcdf(spreaded_mw, lsm, longitude, latitude, lsm_name):
-    """
-    Create default netcdf differential file with time from -26 to 0 and 100 time steps. To modify that use process_time.
-    :param spreaded_mw:
-    :param lsm:
-    :param longitude:
-    :param latitude:
-    :return:
-    """
+        raise ValueError("Invalid mode.")
+    sav_path = f"{output_folder}/{folder_path}/{file_path}"
     
     # m3/s to kg/m2/s
-    processed_mask = m3s_to_kgm2s(spreaded_mw, longitude, latitude)
+    processed_mask = m3s_to_kgm2s(discharge_mw, longitude, latitude)
     
     masked_mw = masking_method(processed_mask, lsm)
     
-    # time fix
-    time = np.arange(-26000, 100, 100)
+    # time processing
+    t_ref = "to_define"
+    processed_mw, processed_time = process_time(masked_mw, start_year, end_year, t_ref)
     
-    #
-    sav_path = f"/nfs/annie/eeymr/work/outputs/Proj_GLAC1D/dif_-26_0/{lsm_name}.qrparm.GLAC1D_DEGLAC.nc"
-    print(f"____ Saving at: {sav_path}")
+    # !!!!!!
     
-    # to netcdf
-    ds = xr.Dataset({'discharge': (('t', 'latitude', 'longitude'), masked_mw)},
-                    coords={'t': time, 'latitude': latitude,
-                            'longitude': longitude})
-    ds['discharge'].attrs['units'] = 'kg m-2 s-1'
-    ds['discharge'].attrs['longname'] = 'P-E FLUX CORRECTION       KG/M2/S  A'
-    # ds['discharge'].attrs['_FillValue'] = 9.96921e+36
-    # ds['discharge'].attrs['missingvalue'] = 9.96921e+36
-    
-    ds['t'].attrs['long_name'] = 'time'
-    ds['t'].attrs['units'] = 'years since 0000-01-01 00:00:00'
-    ds['t'].attrs['calendar'] = '360_days'
-    
-    ds['longitude'].attrs['long_name'] = 'longitude'
-    ds['longitude'].attrs['actual_range'] = '0., 359.'
-    ds['longitude'].attrs['axis'] = 'X'
-    ds['longitude'].attrs['units'] = 'degrees_east'
-    ds['longitude'].attrs['modulo'] = '360'
-    ds['longitude'].attrs['topology'] = 'circular'
-    
-    ds['latitude'].attrs['long_name'] = 'latitude'
-    ds['latitude'].attrs['actual_range'] = '-89.5, 89.5'
-    ds['latitude'].attrs['axis'] = 'y'
-    ds['latitude'].attrs['units'] = 'degrees_north'
-    
-    ds.attrs['title'] = \
-        f"waterfix for transient GLAC1D last delgaciation HadCM3 project - {lsm_name} land sea mask"
-    ds.attrs['history'] = f"Created {datetime.datetime.now()} by Yvan Romé"
-    
-    ds.to_netcdf(sav_path)
-
-
-def spreaded_to_netcdf(spreaded_mw, lsm, longitude, latitude, lsm_name):
-    """
-    Create default netcdf file with time from -26 to 0 and 100 time steps. To modify that use process_time.
-    :param spreaded_mw:
-    :param lsm:
-    :param longitude:
-    :param latitude:
-    :return:
-    """
-    
-    # m3/s to kg/m2/s
-    processed_mask = m3s_to_kgm2s(spreaded_mw, longitude, latitude)
-    
-    masked_mw = masking_method(processed_mask, lsm)
-    
-    # time fix
-    time = np.arange(-26000, 100, 100)
-    
-    #
-    sav_path = f"/nfs/annie/eeymr/work/outputs/Proj_GLAC1D/dif_-26_0_s/{lsm_name}.qrparm.waterfix_GLAC1D_DEGLAC_s.nc"
-    print(f"____ Saving at: {sav_path}")
-    
-    # to netcdf
-    ds = xr.Dataset({'discharge': (('t', 'latitude', 'longitude'), masked_mw)},
-                    coords={'t': time, 'latitude': latitude,
-                            'longitude': longitude})
-    ds['discharge'].attrs['units'] = 'kg m-2 s-1'
-    ds['discharge'].attrs['longname'] = 'P-E FLUX CORRECTION       KG/M2/S  A'
-    # ds['discharge'].attrs['_FillValue'] = 9.96921e+36
-    # ds['discharge'].attrs['missingvalue'] = 9.96921e+36
-    
-    ds['t'].attrs['long_name'] = 'time'
-    ds['t'].attrs['units'] = 'years since 0000-01-01 00:00:00'
-    ds['t'].attrs['calendar'] = '360_days'
-    
-    ds['longitude'].attrs['long_name'] = 'longitude'
-    ds['longitude'].attrs['actual_range'] = '0., 359.'
-    ds['longitude'].attrs['axis'] = 'X'
-    ds['longitude'].attrs['units'] = 'degrees_east'
-    ds['longitude'].attrs['modulo'] = '360'
-    ds['longitude'].attrs['topology'] = 'circular'
-    
-    ds['latitude'].attrs['long_name'] = 'latitude'
-    ds['latitude'].attrs['actual_range'] = '-89.5, 89.5'
-    ds['latitude'].attrs['axis'] = 'y'
-    ds['latitude'].attrs['units'] = 'degrees_north'
-    
-    ds.attrs['title'] = \
-        f"waterfix for transient GLAC1D last delgaciation HadCM3 project - {lsm_name} land sea mask"
-    ds.attrs['history'] = f"Created {datetime.datetime.now()} by Yvan Romé"
-    
-    ds.to_netcdf(sav_path)
-
-
-def corrected_to_netcdf(corrected_mw, lsm, longitude, latitude, lsm_name):
-    """
-    Create default netcdf file with time from -26 to 0 and 100 time steps. To modify that use process_time.
-    :param corrected_mw:
-    :param lsm:
-    :param longitude:
-    :param latitude:
-    :return:
-    """
-    
-    # m3/s to kg/m2/s
-    processed_mask = m3s_to_kgm2s(corrected_mw, longitude, latitude)
-    
-    masked_mw = masking_method(processed_mask, lsm)
-    
-    # time fix
-    time = np.arange(-26000, 100, 100)
-    
-    #
-    sav_path = f"/nfs/annie/eeymr/work/outputs/Proj_GLAC1D/dif_-26_0_sc/{lsm_name}.qrparm.waterfix_GLAC1D_DEGLAC_sc.nc"
-    print(f"____ Saving at: {sav_path}")
-    
-    # to netcdf
-    ds = xr.Dataset({'discharge': (('t', 'latitude', 'longitude'), masked_mw)},
-                    coords={'t': time, 'latitude': latitude,
-                            'longitude': longitude})
-    ds['discharge'].attrs['units'] = 'kg m-2 s-1'
-    ds['discharge'].attrs['longname'] = 'P-E FLUX CORRECTION       KG/M2/S  A'
-    # ds['discharge'].attrs['_FillValue'] = 9.96921e+36
-    # ds['discharge'].attrs['missingvalue'] = 9.96921e+36
-    
-    ds['t'].attrs['long_name'] = 'time'
-    ds['t'].attrs['units'] = 'years since 0000-01-01 00:00:00'
-    ds['t'].attrs['calendar'] = '360_days'
-    
-    ds['longitude'].attrs['long_name'] = 'longitude'
-    ds['longitude'].attrs['actual_range'] = '0., 359.'
-    ds['longitude'].attrs['axis'] = 'X'
-    ds['longitude'].attrs['units'] = 'degrees_east'
-    ds['longitude'].attrs['modulo'] = '360'
-    ds['longitude'].attrs['topology'] = 'circular'
-    
-    ds['latitude'].attrs['long_name'] = 'latitude'
-    ds['latitude'].attrs['actual_range'] = '-89.5, 89.5'
-    ds['latitude'].attrs['axis'] = 'y'
-    ds['latitude'].attrs['units'] = 'degrees_north'
-    
-    ds.attrs['title'] = \
-        f"waterfix for transient GLAC1D last delgaciation HadCM3 project - {lsm_name} land sea mask"
-    ds.attrs['history'] = f"Created {datetime.datetime.now()} by Yvan Romé"
-    
-    ds.to_netcdf(sav_path)
-
-
-def processed_to_netcdf(processed_mw, processed_time, longitude, latitude, mode_smooth, start, end, lsm_name,
-                        corrected=False):
-    """
-    Create default netcdf file with time from -26 to 0 and 100 time steps. To modify that use process_time.
-    :param ds_mw: in kg/m2/s
-    :param lsm:
-    :param longitude:
-    :param latitude:
-    :return:
-    """
-    
-    #
-    corrected_name = (corrected is True) * "c"
-    
-    sav_path = f"/nfs/annie/eeymr/work/outputs/Proj_GLAC1D/{mode_smooth}_{start}_{end}_s{corrected_name}/" \
-               f"{lsm_name}.qrparm.waterfix_GLAC1D_DEGLAC_s{corrected_name}.nc"
-    print(f"____ Saving at: {sav_path}")
-    
-    # to netcdf
     ds = xr.Dataset({'discharge': (('t', 'latitude', 'longitude'), processed_mw)},
                     coords={'t': processed_time, 'latitude': latitude,
                             'longitude': longitude})
@@ -246,25 +62,64 @@ def processed_to_netcdf(processed_mw, processed_time, longitude, latitude, mode_
     ds['latitude'].attrs['axis'] = 'y'
     ds['latitude'].attrs['units'] = 'degrees_north'
     
-    ds.attrs['title'] = \
-        f"waterfix for transient GLAC1D last delgaciation HadCM3 project - {lsm_name} land sea mask"
+    ds.attrs['title'] = title
     ds.attrs['history'] = f"Created {datetime.datetime.now()} by Yvan Romé"
     
+    print(f"____ Saving at: {sav_path}")
     ds.to_netcdf(sav_path)
 
 
-def create_output_folder(mode, start, end, spreaded=True, corrected=False):
-    # Create directory
+def fixing(ds_ref, mode_fixing, mode_smooth, start, end, lsm_name, corrected=False):
+    print("__ Fixing algorithm")
     
-    if mode == "differential":
-        mode_name = "dif"
+    ref_mw, longitude, latitude, t_ref = \
+        ds_ref.discharge.values, ds_ref.longitude.values, ds_ref.latitude.values, ds_ref.t.values
+    
+    if mode_fixing == "time":
+        processed_mw, processed_time = process_time(ref_mw, start, end, t_ref)
+    #         processed_to_netcdf(processed_mw, processed_time, longitude, latitude, mode_smooth, start, end, lsm_name,
+    #                             corrected)
     else:
-        mode_name = "nodif"
+        print("The mode wasn't recognized.")
+
+
+# ------------------------------------ #
+# ---------- SAVING METHODS ---------- #
+# ------------------------------------ #
+
+def routed_to_netcdf(start_year, end_year, step, mode_smooth, lsm_name):
+    folder_path = f"wfix_{start_year}_{end_year}_{step}_{mode_smooth}/"
+    file_path = f"{lsm_name}.qrparm.glac_mw.nc"
     
-    spreaded_name = (spreaded is True) * "s"
-    corrected_name = (corrected is True) * "c"
+    title = f"waterfix for transient GLAC1D last delgaciation HadCM3 simulations " \
+            f"- {lsm_name} land sea mask - {start_year}kya to {end_year}kya with {step}yrs time step " \
+            f"- {mode_smooth} mode processing - no spreading or patch correction."
     
-    dir_name = f"/nfs/annie/eeymr/work/outputs/Proj_GLAC1D/{mode_name}_{start}_{end}_{spreaded_name}{corrected_name}"
+    return folder_path, file_path, title
+
+
+def spreaded_to_netcdf(start_year, end_year, step, mode_smooth, lsm_name):
+    folder_path = f"wfix_{start_year}_{end_year}_{step}_{mode_smooth}s/"
+    file_path = f"{lsm_name}.qrparm.glac_mw.nc"
+    title = f"waterfix for transient GLAC1D last delgaciation HadCM3 simulations " \
+            f"- {lsm_name} land sea mask - {start_year}kya to {end_year}kya with {step}yrs time step " \
+            f"- {mode_smooth} mode processing - spreading applied but no patch correction."
+    
+    return folder_path, file_path, title
+
+
+def corrected_to_netcdf(start_year, end_year, step, mode_smooth, lsm_name):
+    folder_path = f"wfix_{start_year}_{end_year}_{step}_{mode_smooth}sc/"
+    file_path = f"{lsm_name}.qrparm.glac_mw.nc"
+    title = f"waterfix for transient GLAC1D last delgaciation HadCM3 simulations " \
+            f"- {lsm_name} land sea mask - {start_year}kya to {end_year}kya with {step}yrs time step " \
+            f"- {mode_smooth} mode processing - spreading and patch correction applied."
+    
+    return folder_path, file_path, title
+
+
+def create_output_folder(folder_name):
+    dir_name = f"/nfs/annie/eeymr/work/outputs/proj_glac_mw/{folder_name}"
     
     try:
         # Create target Directory
@@ -275,7 +130,8 @@ def create_output_folder(mode, start, end, spreaded=True, corrected=False):
 
 
 def save_corrected_waterfix(ds_wfix, corrected_waterfix, expt_name, start_date, end_date):
-    sav_path = f"/nfs/annie/eeymr/work/outputs/Proj_GLAC1D/corrected_waterfix/{expt_name}.qrparam.waterfix.hadcm3.corrected.nc"
+    sav_path =\
+        f"/nfs/annie/eeymr/work/outputs/Proj_GLAC1D/corrected_waterfix/{expt_name}.qrparam.waterfix.hadcm3.corrected.nc"
     print(f"____ Saving at: {sav_path}")
     
     longitude, latitude, t, depth = ds_wfix.longitude.values, ds_wfix.latitude.values, ds_wfix.t.values, ds_wfix.depth.values
