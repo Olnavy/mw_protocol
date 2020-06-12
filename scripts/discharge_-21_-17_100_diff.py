8bc18c23868f3bb06af75f09d31ed2b44bc6a221
Olnavy <yv.rome@gmail.com>
2020-06-12 20:13:50 +0100

    New scripts.

--- scripts/discharge_-21_-17_100_diff.py
+++ scripts/discharge_-21_-17_100_diff.py
@@ -10,29 +10,29 @@ experiments = ['temev', 'temfa', 'temeq', 'temel', 'temeg', 'temeb', 'temfA', 't
 
 for experiment in experiments:
     ds_routed = xr.open_dataset(
-        f"/nfs/see-fs-01_users/eeymr/work/outputs/proj_glac_mw/glac.-21_0_100.diff/{experiment}.discharge.glac_mw.nc",
+        f"/nfs/see-fs-01_users/eeymr/work/outputs/proj_glac_mw/glac.-26_0_100.diff/{experiment}.discharge.glac_mw.nc",
         decode_times=False)
-    saving.correcting(ds_routed, new_step=500)
+    saving.correcting(ds_routed, new_start_year=-21, new_end_year=-17)
     
     ds_spreaded = xr.open_dataset(
-        f"/nfs/see-fs-01_users/eeymr/work/outputs/proj_glac_mw/glac.-21_0_100.diff_s/{experiment}.discharge.glac_mw.nc",
+        f"/nfs/see-fs-01_users/eeymr/work/outputs/proj_glac_mw/glac.-26_0_100.diff_s/{experiment}.discharge.glac_mw.nc",
         decode_times=False)
-    saving.correcting(ds_spreaded, new_step=500)
+    saving.correcting(ds_spreaded, new_start_year=-21, new_end_year=-14)
     
     # to waterfix
     
     ds_wfix = xr.open_dataset("/nfs/annie/earpal/database/experiments/temev/inidata/temev.qrparm.waterfix.nc")
     
     ds_routed = xr.open_dataset(
-        f"/nfs/see-fs-01_users/eeymr/work/outputs/proj_glac_mw/glac.-21_0_500.diff/{experiment}.discharge.glac_mw.nc",
+        f"/nfs/see-fs-01_users/eeymr/work/outputs/proj_glac_mw/glac.-21_-17_100.diff/{experiment}.discharge.glac_mw.nc",
         decode_times=False)
     saving.to_waterfix(ds_routed, ds_wfix)
     
     ds_spreaded = xr.open_dataset(
-        f"/nfs/see-fs-01_users/eeymr/work/outputs/proj_glac_mw/glac.-21_0_500.diff_s/{experiment}.discharge.glac_mw.nc",
+        f"/nfs/see-fs-01_users/eeymr/work/outputs/proj_glac_mw/glac.-21_-17_100.diff_s/{experiment}.discharge.glac_mw.nc",
         decode_times=False)
     saving.to_waterfix(ds_spreaded, ds_wfix)
 
 plotting.plot_discharge_ts(
-    "/nfs/see-fs-01_users/eeymr/work/outputs/proj_glac_mw/glac.-21_0_500.diff_s/temev.discharge.glac_mw.nc",
+    "/nfs/see-fs-01_users/eeymr/work/outputs/proj_glac_mw/glac.-21_-17_100.diff_s/temev.discharge.glac_mw.nc",
     unit="kg/m2/s", out="save")
